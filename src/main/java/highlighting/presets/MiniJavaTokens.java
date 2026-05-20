d1b6ae8ef6a06db349c318ad650a1073fb49d70e
phanabel123-rgb <phan.abel123@gmail.com>
2026-05-20 19:01:26 +0200

    MiniJavaTokens vervollständigt

--- src/main/java/highlighting/presets/MiniJavaTokens.java
+++ src/main/java/highlighting/presets/MiniJavaTokens.java
@@ -19,10 +19,25 @@ public final class MiniJavaTokens {
   public static List<Token> defaultTokens() {
     return List.of(
         // Example: string literals (students should define further tokens below)
-        Token.of(Pattern.compile("\"([^\"\\\\]|\\\\.)*\""), MiniJavaColours.STRING_LITERAL_COLOUR)
+        Token.of(Pattern.compile("\"([^\"\\\\]|\\\\.)*\""), MiniJavaColours.STRING_LITERAL_COLOUR),
 
         // TODO: Define additional tokens for MiniJava, e.g. character literals, keywords,
         // annotations, comments, identifiers, numbers, operators, etc.
+
+        Token.of(Pattern.compile("'([^\"\\\\]|\\\\.)*'"), MiniJavaColours.CHAR_LITERAL_COLOUR),
+
+        Token.of(Pattern.compile("\\b(package|public|final|class|private" +
+            "|import|extends|String|return|void|null|new|char)\\b"), MiniJavaColours.KEYWORD_COLOUR),
+
+        Token.of(Pattern.compile("@[\\w-]+"), MiniJavaColours.ANNOTATION_COLOUR),
+
+        Token.of(Pattern.compile("//.*"), MiniJavaColours.LINE_COMMENT_COLOUR),
+
+        Token.of(Pattern.compile("/\\*(?s).*?\\*/"), MiniJavaColours.BLOCK_COMMENT_COLOUR),
+
+        Token.of(Pattern.compile("/\\*\\*(?s).*?\\*/"), MiniJavaColours.JAVADOC_COMMENT_COLOUR)
+
         );
+
   }
 }
